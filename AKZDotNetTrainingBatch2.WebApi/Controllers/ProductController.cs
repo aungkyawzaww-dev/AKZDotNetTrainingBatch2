0a87e82901b7d979a51e2734a0a226e4357d74dc
Aung Kyaw Zaww <aungkyawzaww7889@gmail.com>
2025-07-20 04:26:30 +0630

    feat: api frame built

---
+++ AKZDotNetTrainingBatch2.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AKZDotNetTrainingBatch2.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            //process
+            //data
+            return Ok("Get product");
+        }
+
+        [HttpPost]
+        public IActionResult Create()
+        {
+            //process
+            //data
+            return Ok("Create product");
+        }
+
+        [HttpPut]
+        public IActionResult Upset()
+        {
+            //process
+            //data
+            return Ok("Upsert product");
+        }
+
+        [HttpPatch]
+        public IActionResult Update()
+        {
+            //process
+            //data
+            return Ok("Update product");
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            //process
+            //data
+            return Ok("Delete product");
+        }
+
+    }
+}
